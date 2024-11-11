fdc3d2f2e1c8ee6491461e6f7c0081f6fc0e9419
luizfcneto <luizfcneto123@gmail.com>
2024-11-11 12:44:07 -0300

    +Game Controller, Service, Repository, and MinDTO

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+	
+	public GameMinDTO() {};
+	
+	public GameMinDTO(Game gameEntity) {
+		this.id = gameEntity.getId();
+		this.title = gameEntity.getTitle();
+		this.year = gameEntity.getYear();
+		this.imgUrl = gameEntity.getImgUrl();
+		this.shortDescription = gameEntity.getShortDescription();
+	}
+	
+	public GameMinDTO(Long id, String title, Integer year, String imgUrl, String shortDescription) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.year = year;
+		this.imgUrl = imgUrl;
+		this.shortDescription = shortDescription;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+		
+}
